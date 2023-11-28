d46e6e71a9432971b9d96baf0652109f92572a80
ntorrie <ntorrie@perennia.ca>
2023-11-28 13:14:03 -0400

    create roxygen skeleton documentation for assign_trim_times_sal functions

--- R/assign_trim_times_sal.R
+++ R/assign_trim_times_sal.R
@@ -1,8 +1,16 @@
+#' Create variable for SAL test start time in UTC 
+#'
+#' @param Log 
+#'
+#' @return Returns SALstarttime_utc
+#' @export
+#'
+
+
 #Salinity
 #Convert the test start/end times to UTC 
 #Add 3 hrs to convert from ADT (dst true). Daylight savings (ADT) runs roughly from March 12- November 5.
 #Add 4 hrs to convert from AST (dst false)
-
 
 assign_trim_start_sal <- function(Log){
   SALLog <- filter(Log, validation_variable == "SAL") %>%
@@ -10,6 +18,14 @@ assign_trim_start_sal <- function(Log){
   SALstarttime_utc <- as_datetime(SALLog$deployment_datetime, tz = "UTC")
 }
 
+
+#' Create variable for SAL test end time in UTC 
+#'
+#' @param Log 
+#'
+#' @return Returns SALendtime_utc
+#' @export
+#'
 
 assign_trim_end_sal <- function(Log){
   SALLog <- filter(Log, validation_variable == "SAL") %>%
