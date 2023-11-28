8b1b969f95b2824b8bb8eee65632bf3224a9b454
ntorrie <ntorrie@perennia.ca>
2023-11-28 13:38:16 -0400

    add roxygen documentation for clean_log_all

--- R/clean_log_all.R
+++ R/clean_log_all.R
@@ -1,5 +1,17 @@
-#clean_log_all to run #unify log dates and remove duplicate log items & create a log which sensorstrings can import
+#' Clean log for export
+#'
+#' @param Log Metadata log 
+#'
+#' @return Returns cleaned log which sensorstrings can import
+#' 
+#' @author Nicole Torrie
+#' 
+#' @export
+#'
+#' 
 
+
+#unify log dates and remove duplicate log items in order to create a log which sensorstrings can import
 
 clean_log_all <- function(Log = Log) {
   Log <- unify_log_dates(Log)
