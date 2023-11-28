50b8af4787c84ce8432db9ac220f4348392bcef9
ntorrie <ntorrie@perennia.ca>
2023-11-28 13:10:45 -0400

    remove old code from assign_trim_times_sal

--- R/assign_trim_times_sal.R
+++ R/assign_trim_times_sal.R
@@ -1,10 +1,9 @@
 #Salinity
-#Convert the test start/end times to UTC https://savvytime.com/converter/ast-to-utc
+#Convert the test start/end times to UTC 
 #Add 3 hrs to convert from ADT (dst true). Daylight savings (ADT) runs roughly from March 12- November 5.
 #Add 4 hrs to convert from AST (dst false)
 
 
-#new method (tz)
 assign_trim_start_sal <- function(Log){
   SALLog <- filter(Log, validation_variable == "SAL") %>%
     filter(row_number()==1)
@@ -18,28 +17,3 @@ assign_trim_end_sal <- function(Log){
   SALendtime_utc <- as_datetime(SALLog$retrieval_datetime, tz = "UTC")
 }
 
-
-
-
-#old method (manual)
-
-# assign_trim_start_sal <- function(Log){
-#   dst <- dst(Log[1,]$deployment_datetime)
-#   ifelse(dst == TRUE,  adjusttime <- 3*60*60, adjusttime <- 4*60*60)
-#   SALLog <- filter(Log, validation_variable == "SAL") %>% 
-#     filter(row_number()==1)
-#   SALstarttime_utc <- SALLog$deployment_datetime + adjusttime
-# }
-# 
-# 
-# assign_trim_end_sal <- function(Log){
-#   dst <- dst(Log[1,]$deployment_datetime)
-#   ifelse(dst == TRUE,  adjusttime <- 3*60*60, adjusttime <- 4*60*60)
-#   SALLog <- filter(Log, validation_variable == "SAL") %>% 
-#     filter(row_number()==1)
-#   SALendtime_utc <- SALLog$retrieval_datetime + adjusttime
-# }
-
-
-# SALstarttime_utc <- assign_trim_start_sal(Log)
-# SALendtime_utc <- assign_trim_end_sal(Log)
