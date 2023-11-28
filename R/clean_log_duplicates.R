02b741a3884546912c47d6195a2720ccb1cbafda
ntorrie <ntorrie@perennia.ca>
2023-11-28 13:48:12 -0400

    Updated documentation for clean_log_duplicates

--- R/clean_log_duplicates.R
+++ R/clean_log_duplicates.R
@@ -1,6 +1,20 @@
-#clean_log_duplicates
-#separate duplicate rows from multi variable sensors in LOG
+#' Clean duplicate rows out of metadata log
+#'
+#' @param Log Metadata log
+#'
+#' @return Returns cleaned log with no duplicate rows from multi-variable sensors
+#' 
+#' @author Nicole Torrie
+#' 
+#' @importFrom dplyr distinct rename
+#' 
+#' @export
+#'
 
+
+
+#clean_log_duplicates
+#remove duplicate rows from multi variable sensors in Log
 clean_log_duplicates <- function(Log) {
   cleaned_log <-
     data.frame(distinct(Log, `Serial#`, .keep_all = TRUE)) %>%
@@ -9,4 +23,3 @@ clean_log_duplicates <- function(Log) {
 }
 
 
-#Log2 <- clean_log_duplicates(Log)
