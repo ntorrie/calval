3ae57aa68116e89c8c4a9fb55fd6c52d55254a26
ntorrie <ntorrie@perennia.ca>
2023-11-28 13:08:46 -0400

    generate roxygen skeleton for assign_trim_times_do functions

--- R/assign_trim_times_do.R
+++ R/assign_trim_times_do.R
@@ -1,16 +1,14 @@
-
-
-#' Title
+#' Create variable for DO test start time in UTC 
 #'
 #' @param Log 
 #'
-#' @return
+#' @return Returns DOstarttime_utc
 #' @export
 #'
-#' @examples
+
 
 #Dissolved Oxygen
-#Convert the test start/end times to UTC https://savvytime.com/converter/ast-to-utc
+#Convert the test start/end times to UTC 
 #Add 3 hrs to convert from ADT (dst true). Daylight savings (ADT) runs roughly from March 12- November 5.
 #Add 4 hrs to convert from AST (dst false)
 
@@ -19,6 +17,15 @@ assign_trim_start_do <- function(Log){
     filter(row_number()==1)
   DOstarttime_utc <- as_datetime(DOLog$deployment_datetime, tz = "UTC")
 }
+
+
+#' Create variable for DO test end time in UTC
+#'
+#' @param Log 
+#'
+#' @return Returns DOendtime_utc
+#' @export
+#'
 
 
 assign_trim_end_do <- function(Log){
