27cb9be26a3085dfa7d57b2394978348cfaef951
ntorrie <ntorrie@perennia.ca>
2023-11-29 11:43:57 -0400

    add functions for plotting hobo DO data

---
+++ R/ggplot_hdo_flag.R
@@ -0,0 +1,33 @@
+#' Plot HOBO DO validation data, colorize points by HOBO DO flag
+#'
+#' @param final_HDO Dataframe filtered for HDO test data
+#'
+#' @return Returns a ggplot object of HOBO DO validation data colorized based on HOBO DO flag
+#' 
+#' @author Nicole Torrie
+#' 
+#' @importFrom ggplot2 ggplot aes geom_point scale_color_manual geom_line geom_ribbon labs
+#' 
+#' @export
+#'
+
+
+ggplot_hdo_flag <- function(final_HDO) {
+  v <-
+    ggplot(final_HDO,
+           aes(
+             x = timestamp_utc,
+             y = dissolved_oxygen_uncorrected_mg_per_l,
+             color = as.factor(FLAG)
+           )) +
+    geom_point(size = 0.25) +
+    scale_color_manual(values = c('Green', 'Red')) +
+    geom_line(aes(y = median), color = "black") +
+    geom_ribbon(aes(ymin = median - 0.2, ymax = median + 0.2),
+                alpha = 0.3,
+                color = NA)
+  
+  v <- v  + labs(color = 'FLAG')
+  v
+}
+
---
+++ R/ggplot_hdo_val.R
@@ -0,0 +1,33 @@
+#' Plot HOBO DO validation data, colorize points by sensor serial #
+#'
+#' @param final_HDO Dataframe filtered for HOBO DO test data
+#'
+#' @return Returns a ggplot object of HOBO DO validation data colorized based on sensor serial #
+#' 
+#' @author Nicole Torrie
+#' 
+#' @importFrom ggplot2 ggplot aes geom_point geom_line geom_ribbon labs guides guide_legend
+#' 
+#' @export
+#'
+
+ggplot_hdo_val <- function(final_HDO) {
+  w <-
+    ggplot(final_HDO,
+           aes(
+             x = timestamp_utc,
+             y = dissolved_oxygen_uncorrected_mg_per_l,
+             color = as.factor(sensor_serial_number)
+           )) +
+    geom_point(size = 0.25) +
+    geom_line(aes(y = median), color = "black") +
+    geom_ribbon(aes(ymin = median - 0.2, ymax = median + 0.2),
+                alpha = 0.3,
+                color = NA) 
+  
+  
+  w <- w  + labs(color = 'sensor_serial_number') +
+    guides(fill = guide_legend(ncol = 2))
+  w
+}
+
