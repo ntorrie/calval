d97fdcc25180793319a155e1a30a1b859fc238a7
ntorrie <ntorrie@perennia.ca>
2024-04-23 12:28:38 -0300

    Increased geom_point size to make it easier to intepret data plot

--- R/ggplot_temp_val.R
+++ R/ggplot_temp_val.R
@@ -19,7 +19,7 @@ ggplot_temp_val <- function(final_temp) {
              y = temperature_degree_c,
              color = as.factor(sensor_serial_number)
            )) +
-    geom_point(size = 0.25) +
+    geom_point(size = 0.5) +
     geom_line(aes(y = median), color = "black") +
     geom_ribbon(aes(ymin = median - 0.2, ymax = median + 0.2),
                 alpha = 0.3,
