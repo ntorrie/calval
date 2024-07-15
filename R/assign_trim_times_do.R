2fcbc1708471b43d542d3b7a36b2880d349bedb3
ntorrie <ntorrie@perennia.ca>
2024-07-15 13:38:59 -0300

    Update case and naming for consistency

--- R/assign_trim_times_do.R
+++ R/assign_trim_times_do.R
@@ -1,6 +1,6 @@
 #' Create variable for DO test start time in UTC 
 #'
-#' @param Log Metadata log with deployment_datetime column
+#' @param log Metadata log with deployment_datetime column
 #'
 #' @return Returns DOstarttime_utc
 #' 
@@ -18,16 +18,16 @@
 #Add 3 hrs to convert from ADT (dst true). Daylight savings (ADT) runs roughly from March 12- November 5.
 #Add 4 hrs to convert from AST (dst false)
 
-assign_trim_start_do <- function(Log){
-  DOLog <- filter(Log, validation_variable == "DO", Logger_Model == "aquaMeasure DOT") %>%
-    filter(row_number()==1)
-  DOstarttime_utc <- as_datetime(DOLog$deployment_datetime, tz = "UTC")
+assign_trim_start_do <- function(log){
+  dolog <- filter(log, validation_variable == "DO", Logger_Model == "aquaMeasure DOT") %>%
+    filter(row_number() == 1)
+  DOstarttime_utc <- as_datetime(dolog$deployment_datetime, tz = "UTC")
 }
 
 
 #' Create variable for DO test end time in UTC
 #'
-#' @param Log Metadata log with deployment_datetime column
+#' @param log Metadata log with deployment_datetime column
 #'
 #' @return Returns DOendtime_utc
 #' 
@@ -40,8 +40,8 @@ assign_trim_start_do <- function(Log){
 #'
 
 
-assign_trim_end_do <- function(Log){
-  DOLog <- filter(Log, validation_variable == "DO", Logger_Model == "aquaMeasure DOT") %>%
-    filter(row_number()==1)
-  DOendtime_utc <- as_datetime(DOLog$retrieval_datetime, tz = "UTC")
+assign_trim_end_do <- function(log){
+  dolog <- filter(log, validation_variable == "DO", Logger_Model == "aquaMeasure DOT") %>%
+    filter(row_number() == 1)
+  DOendtime_utc <- as_datetime(dolog$retrieval_datetime, tz = "UTC")
 }
